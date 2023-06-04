7dff7b406fe448da8fe5d200a19ff110d0b1408b
Mathew Ostrander <mathew.ostrander@gmail.com>
2023-06-03 21:46:34 -0500

    quick style improvements

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,10 @@
     console.log("howdy!");
   })();
 
-  function displaySection(sectionName) {
-    document.getElementById('splash').classList = "hidden";
-    document.getElementById('about').classList = "hidden";
-    document.getElementById('resume').classList = "hidden";
-    document.getElementById(sectionName).classList = "show";
-
-    // var sections = document.getElementsByTagName('section');
-    // for (let section of sections) {
-    //     console.log(section);
-    //     section.classList = 'hidden';
-
-    // }
-
-    // document.getElementById(sectionName).classList = "show";
-  }
+function displaySection(sectionName) {
+  document.getElementById('splash').classList = "hidden";
+  document.getElementById('about').classList = "hidden";
+  document.getElementById('resume').classList = "hidden";
+  document.getElementById(sectionName).classList = "show";
+}
 
